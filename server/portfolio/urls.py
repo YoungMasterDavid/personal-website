fe6fcd1d768989c29976dbe45331a7961fe9dab6
YoungMasterDavid <dcporras@up.edu.ph>
2025-07-20 22:04:05 +0800

    admin

--- server/portfolio/urls.py
+++ server/portfolio/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProjectListAPIView, ContactMessageAPIView
+from .views import ProjectListAPIView, ContactMessageAPIView, create_admin
 
 urlpatterns = [
     path('projects/', ProjectListAPIView.as_view()),
     path('contact/', ContactMessageAPIView.as_view()),  # new contact endpoint
+    path('create-admin/', create_admin)
 ]
--- server/portfolio/views.py
+++ server/portfolio/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,6 +8,7 @@ from .models import Project
 from .serializers import ProjectSerializer
 from rest_framework import status
 from .serializers import ContactMessageSerializer
+from django.contrib.auth.models import User
 
 class ProjectListAPIView(APIView):
     def get(self, request):
@@ -21,3 +23,8 @@ class ContactMessageAPIView(APIView):
             serializer.save()
             return Response({'message': 'Message received!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
+    return JsonResponse({'status': 'admin created'})
