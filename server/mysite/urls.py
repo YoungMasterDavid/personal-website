6641bf85f2f2f0de97013ee4db33e5eeed6c3ee9
YoungMasterDavid <dcporras@up.edu.ph>
2025-07-20 22:09:49 +0800

    create admin 2

--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
+    path('create-admin/', include('portfolio.urls')),  # Admin creation endpoint
 ]
