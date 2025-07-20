28827b4ba451117ee36bdb3c37b07406888b1c74
YoungMasterDavid <dcporras@up.edu.ph>
2025-07-20 22:14:53 +0800

    create_admin 3

--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from portfolio.views import create_admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('portfolio.urls')),
-    path('create-admin/', include('portfolio.urls')),  # Admin creation endpoint
+    path('create-admin/', create_admin),  # Admin creation endpoint
 ]
--- server/portfolio/urls.py
+++ server/portfolio/urls.py
@@ -4,5 +4,4 @@ from .views import ProjectListAPIView, ContactMessageAPIView, create_admin
 urlpatterns = [
     path('projects/', ProjectListAPIView.as_view()),
     path('contact/', ContactMessageAPIView.as_view()),  # new contact endpoint
-    path('create-admin/', create_admin)
 ]
